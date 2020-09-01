25aafcf798c7ba323e6a2aa00b075df5526f54e8
Nick Qantaria <skott56@outlook.com>
2020-09-01 13:18:06 +0400

    MathTests with TestCase attribute, SetUp attribute for creating new instances

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void Setup()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_OnCall_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnGreatherArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
