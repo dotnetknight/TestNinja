7f3b601f90430d703b451a5fc3cfa5f519d2e3ed
Nick Qantaria <skott56@outlook.com>
2021-07-02 10:38:48 +0400

    video sevice and mocking

---
+++ TestNinja.UnitTests/VideoServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        private VideoService service;
+        private Mock<IFileReader> fileReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            fileReader = new Mock<IFileReader>();
+            service = new VideoService(fileReader.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+            
+            var result = service.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
